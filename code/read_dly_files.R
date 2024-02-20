734e73baab363c4218dc06fdfa5c30a3b5265025
unknown <tannermbarnes@gmail.com>
2024-02-20 18:34:26 -0500

    read in files

---
+++ code/read_dly_files.R
@@ -0,0 +1,32 @@
+library(tidyverse)
+library(glue)
+library(lubridate)
+
+quadruple <- function(x){
+
+    c(glue("VALUE{x}"), glue("MFLAG{x}"), glue("QFLAG{x}"), glue("SFLAG{x}"))
+
+}
+widths <- c(11, 4, 2, 4, rep(c(5, 1, 1, 1), 31))
+headers <- c("ID", "YEAR", "MONTH", "ELEMENT", unlist(map(1:31, quadruple)))
+
+dly_files <- list.files("data/ghcnd_all", full.names = TRUE)
+
+
+read_fwf(dly_files,
+    fwf_widths(widths, headers), 
+    na = c("NA", "-9999"),
+    col_types = cols(.default = col_character()),
+    col_select = c(ID, YEAR, MONTH, ELEMENT, starts_with("VALUE"))) %>% 
+    rename_all(tolower) %>% 
+    filter(element == "PRCP") %>%
+    select(-element) %>%
+    pivot_longer(cols = starts_with("value"), names_to = "day", values_to = "prcp") %>%
+    drop_na() %>%
+    mutate(day = str_replace(day, "value", ""),
+            date = ymd(glue("{year}-{month}-{day}")),
+            prcp = as.numeric(prcp)/100) %>% #prcp now in cm
+    select(id, date, prcp) %>%
+    write_tsv("data/composite_dly.tsv")
+
+
